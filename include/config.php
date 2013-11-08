070061a046d74f709ba973efcd5dd902f7b103bc
yavsophearin <yavsophearin@gmail.com>
2013-11-08 19:53:57 +0700

    delete project

--- include/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	defined("SERVER")?null:define("SERVER_NAME","localhost");
-	defined("USER")?null:define("USER_NAME","root");
-	defined("PASS")?nul:define("PASSWORD","");
-	defined("DATABASE")?null:define("DB_NAME","itcam_db");
-?>
--- include/initailize.php
+++
@@ -1,8 +0,0 @@
-<?php
-	defined("DS")?null:define("DS",DIRECTORY_SEPARATOR);
-	defined("LIB_PATH")?null : define("LIB_PATH",dirname(__FILE__));
-	
-	require_once(LIB_PATH.DS."config.php");
-	require_once(LIB_PATH.DS."database.php");
-	require_once(LIB_PATH.DS."function.php");
-?>
